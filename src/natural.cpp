e334428c034d58b891e46b37c3efb14fc5480cef
SergeJohanns <serge.johanns@gmail.com>
2020-04-30 15:21:19 +0200

    Expanded natural type

--- src/natural.cpp
+++ src/natural.cpp
@@ -4,40 +4,6 @@
 #include "natural.hpp"
 #include "constants.hpp"
 
-Natural::Natural(std::string constant) {
-    for (int i = constant.size() - 1; i >= 0; i++) {
-        number.push_back(constant[i]);
-    }
-}
-
-Natural Natural::operator+(Natural other) {
-    auto out = Natural("0");
-    add_vectors(out.number, this->number);
-    add_vectors(out.number, other.number);
-    return out;
-}
-
-Natural Natural::operator-(Natural other) {
-    auto out = Natural("0");
-    sub_vectors(out.number, this->number);
-    sub_vectors(out.number, other.number);
-    return out;
-}
-
-void add_vectors(std::vector<int> &a, std::vector<int> &b) {
-    int offset = 0;
-    for (auto n : b) {
-        add_to_vectors(a, n, offset++);
-    }
-}
-
-void sub_vectors(std::vector<int> &a, std::vector<int> &b) {
-    int offset = 0;
-    for (auto n : b) {
-        sub_from_vectors(a, n, offset++);
-    }
-}
-
 void add_to_vectors(std::vector<int> &vector, int scalar, int offset) {
     int i = offset;
     int rem = scalar;
@@ -66,6 +32,56 @@ void sub_from_vectors(std::vector<int> &vector, int scalar, int offset) {
         }
     }
     if (rem < 0) {
-        vector = std::vector({0});
+        vector = std::vector<int>({0});
     }
+}
+
+Natural::Natural() {
+
+}
+
+Natural::Natural(std::string constant) {
+    for (int i = constant.size() - 1; i >= 0; i++) {
+        number.push_back(constant[i]);
+    }
+}
+
+void add_vectors(std::vector<int> &a, std::vector<int> &b) {
+    int offset = 0;
+    for (auto n : b) {
+        add_to_vectors(a, n, offset++);
+    }
+}
+
+void sub_vectors(std::vector<int> &a, std::vector<int> &b) {
+    int offset = 0;
+    for (auto n : b) {
+        sub_from_vectors(a, n, offset++);
+    }
+}
+
+Natural Natural::zero() {
+    return Natural("0");
+}
+
+bool Natural::operator==(Natural other) {
+    return this->number == other.number;
+}
+
+bool Natural::operator!=(Natural other) {
+    return this->number != other.number;
+}
+
+Natural Natural::operator+(Natural other) {
+    auto out = Natural::zero();
+    add_vectors(out.number, this->number);
+    add_vectors(out.number, other.number);
+    return out;
+}
+
+Natural Natural::operator-(Natural other) {
+    auto out = Natural("0");
+    sub_vectors(out.number, this->number);
+    sub_vectors(out.number, other.number);
+    return out;
 }
--- src/natural.hpp
+++ src/natural.hpp
@@ -7,10 +7,14 @@
 class Natural {
     public:
         std::vector<int> number;
+        Natural();
         Natural(std::string constant);
+        static Natural zero();
         std::string to_string();
         Natural operator +(Natural other);
         Natural operator -(Natural other);
+        bool operator ==(Natural other);
+        bool operator !=(Natural other);
 };
 
 #endif
