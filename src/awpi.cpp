e73946e18db1de7e494db3281276b643247b24ab
SergeJohanns <serge.johanns@gmail.com>
2020-05-01 00:32:54 +0200

    Placeholder godel options

--- src/awpi.cpp
+++ src/awpi.cpp
@@ -4,12 +4,6 @@
 #include "parser.hpp"
 #include "interpreter.hpp"
 
-void display_state(state prog) {
-    for (auto elem : prog) {
-        std::cout << elem.first + " = " + elem.second.to_string() << std::endl;
-    }
-}
-
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         // No arguments given
@@ -21,14 +15,16 @@ int main(int argc, char* argv[]) {
             std::cout << "No source file given." << std::endl;
             return ERROR_STATUS;
         }
-        lex(run(argv[0], argv[2]));
+        std::cout << "Not yet implemented" << std::endl;
+        //lex(run(argv[0], argv[2]));
     } else if (argv[1] == "-G") {
         // Compute Godel number
         if (argc < 3) {
             std::cout << "No source file given." << std::endl;
             return ERROR_STATUS;
         }
-        lex(run(argv[0], argv[2]));
+        std::cout << "Not yet implemented" << std::endl;
+        //lex(run(argv[0], argv[2]));
     } else {
         // Run file
         display_state(exec(lex(run(argv[0], argv[1]))));
