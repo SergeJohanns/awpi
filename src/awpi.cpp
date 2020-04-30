1afc7eb26108e6e50065549671478e5ab7d2a85c
SergeJohanns <serge.johanns@gmail.com>
2020-04-30 21:10:32 +0200

    Split off main file

---
+++ src/awpi.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "constants.hpp"
+#include "lexer.hpp"
+#include "parser.hpp"
+#include "interpreter.hpp"
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        // No arguments given
+        std::cout << "No source file given." << std::endl;
+        return ERROR_STATUS;
+    } else if (argv[1] == "-g") {
+        // Compute Godelization
+        if (argc < 3) {
+            std::cout << "No source file given." << std::endl;
+            return ERROR_STATUS;
+        }
+        lex(run(argv[0], argv[2]));
+    } else if (argv[1] == "-G") {
+        // Compute Godel number
+        if (argc < 3) {
+            std::cout << "No source file given." << std::endl;
+            return ERROR_STATUS;
+        }
+        lex(run(argv[0], argv[2]));
+    } else {
+        // Run file
+        display_state(exec(lex(run(argv[0], argv[1]))));
+    }
+}
+
+void display_state(state prog) {
+    for (auto elem : prog) {
+        std::cout << elem.first + " = " + elem.second.to_string() << std::endl;
+    }
+}
