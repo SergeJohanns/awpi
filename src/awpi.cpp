f2483ce98ff13f4fbcb158563a5dfa9c6edb5357
SergeJohanns <serge.johanns@gmail.com>
2020-07-14 01:29:37 +0200

    Split godel branch

--- src/awpi.cpp
+++ src/awpi.cpp
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <iostream>
 #include "constants.hpp"
 #include "lexer.hpp"
@@ -9,22 +10,6 @@ int main(int argc, char* argv[]) {
         // No arguments given
         std::cout << "No source file given." << std::endl;
         return ERROR_STATUS;
-    } else if (argv[1] == "-g") {
-        // Compute Godelization
-        if (argc < 3) {
-            std::cout << "No source file given." << std::endl;
-            return ERROR_STATUS;
-        }
-        std::cout << "Not yet implemented" << std::endl;
-        //lex(run(argv[0], argv[2]));
-    } else if (argv[1] == "-G") {
-        // Compute Godel number
-        if (argc < 3) {
-            std::cout << "No source file given." << std::endl;
-            return ERROR_STATUS;
-        }
-        std::cout << "Not yet implemented" << std::endl;
-        //lex(run(argv[0], argv[2]));
     } else {
         // Run file
         display_state(exec(lex(parse(argv[0], argv[1]))));
